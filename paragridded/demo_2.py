224d0dbee80462697ab6c303b891b654abca049a
pvthinker <roullet@univ-brest.fr>
2020-11-15 19:56:30 +0100

    Improve mount/unmount tar files + add demo_2 and statistics

---
+++ paragridded/demo_2.py
@@ -0,0 +1,10 @@
+"""
+Illustrate how to mount/unmount tar files
+"""
+import giga_tools as giga
+
+giga.mount_stats()
+giga.umount_all()
+giga.mount_stats()
+giga.mount_all()
+giga.mount_stats()
--- paragridded/giga_tools.py
+++ paragridded/giga_tools.py
@@ -4,6 +4,7 @@ import os
 import glob
 from shapely.geometry.polygon import Polygon
 import pickle
+from pretty import BB
 
 subdomains = range(1, 14)
 
@@ -31,16 +32,17 @@ def hisfiles(tile):
     # add time in the arguments => 000072
     if tile in subdmap.keys():
         subd = subdmap[tile]
-        return f"{dirmounted}/{subd:02}bis/gigatl1_his.000072.{tile:04}.nc"
+        filename = f"{dirmounted}/{subd:02}/gigatl1_his.000072.{tile:04}.nc"
     else:
-        return ""
+        filename = ""
+    return filename
 
 
 def get_subdmap():
     """Reconstruct how netCDF files are stored in *.tar files"""
     subdmap = {}
     for subd in subdomains:
-        fs = glob.glob(f"{dirmounted}/{subd:02}bis/*nc")
+        fs = glob.glob(f"{dirmounted}/{subd:02}/*nc")
         tiles = [int(f.split(".")[2]) for f in fs]
         for t in tiles:
             subdmap[t] = subd
@@ -66,18 +68,84 @@ def mount_tar(source, template, subd, dest, overwrite=False):
     whichmount = os.popen(f"which {mount}").read()
     assert len(whichmount) > 0, msg
 
-    destdir = f"{dest}/{subd:02}bis"
+    tomount = True
+    destdir = f"{dest}/{subd:02}"
     if os.path.exists(destdir):
         print(f"{destdir} already exists")
-        if overwrite:
-            print("remove its content")
-            os.removedirs(destdir)
+        if len(os.listdir(f"{destdir}")) == 0:
+            # folder is empty
+            pass
+        elif overwrite:
+            # folder is not empty but we want to overwrite it
+            # first let's unmount it
+            command = f"fusermount -u {destdir}"
+            os.system(command)
+            #
+            assert len(os.listdir(f"{destdir}")) == 0
         else:
-            return
-    os.makedirs(destdir)
+            tomount = False
+    else:
+        os.makedirs(destdir)
 
-    command = f"{mount} {srcfile} {destdir}"
-    os.system(command)
+    if tomount:
+        command = f"{mount} {srcfile} {destdir}"
+        os.system(command)
+
+
+def mount(subd):
+    """Mount tar file `subd`"""
+    destdir = f"{dirmounted}"
+    mount_tar(dirgiga, tartemplate, subd, destdir)
+
+
+def mount_all():
+    for subd in subdomains:
+        mount(subd)
+
+
+def mount_stats():
+    """ Print statistics on mounted tar files"""
+
+    print("-"*40)
+    print(BB("statistics on mounted tar files"))
+    print(f"mounting point: {dirmounted}")
+    for subd in subdomains:
+        destdir = f"{dirmounted}/{subd:02}"
+        if os.path.exists(destdir):
+            filelist = os.listdir(f"{destdir}")
+            nbfiles = len(filelist)
+            if nbfiles > 0:
+                tiles = set([int(f.split(".")[-2]) for f in filelist])
+                nbtiles = len(tiles)
+                tile = list(tiles)[0]
+                fs = [f for f in filelist if f"{tile:04}.nc" in f]
+                dateindex = sorted([int(f.split(".")[-3]) for f in fs])
+                nbdates = len(dateindex)
+                print(
+                    f"  - {subd:02} : {nbtiles:03} tiles x {dateindex} dateindex")
+        else:
+            warning = BB("destroyed")
+            print(f"  - {subd:02} : {warning}")
+
+
+def umount_all():
+    for subd in subdomains:
+        umount(subd)
+
+
+def umount(subd):
+    """ Unmount `subd` tar archive folder
+
+    The command to unmount a fuse folder is fusermount -u"""
+
+    destdir = f"{dirmounted}/{subd:02}"
+    if len(os.listdir(f"{destdir}")) != 0:
+        command = f"fusermount -u {destdir}"
+        os.system(command)
+        command = f"rmdir {destdir}"
+        os.system(command)
+    else:
+        print(f"{destdir} is already umounted")
 
 
 def LLTP2domain(lowerleft, topright):
