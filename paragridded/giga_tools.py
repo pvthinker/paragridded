afc26c653de64ff74f6ec9ff35b18b823cc050e2
pvthinker <roullet@univ-brest.fr>
2020-11-17 22:05:15 +0100

    Improve giga_tools to make it work on the TGCC HPC center

--- paragridded/giga_tools.py
+++ paragridded/giga_tools.py
@@ -1,4 +1,10 @@
 """ GIGATL experiment specifications
+
+ dimpart contains
+
+   - "netcdfdimnames": how to map netCDF to CROCO dimensions
+   - "domainpartition": which CROCO dimensions are tiled
+
 """
 import os
 import glob
@@ -16,15 +22,55 @@ dimpart = {"netcdfdimnames":
             "xi": ("xi_rho", "xi_u")},
            "domainpartition": ("eta", "xi")}
 
-dirgrid = "/net/omega/local/tmp/1/gula/GIGATL1/GIGATL1_1h_tides/GRD"
-dirsurf = "/net/omega/local/tmp/1/gula/GIGATL1/GIGATL1_1h_tides/SURF/gigatl1_surf.2008-05-23"
-dirgiga = "/net/omega/local/tmp/1/gula/GIGATL1/GIGATL1_1h_tides/HIS_1h"
-dirmounted = "/net/omega/local/tmp/1/roullet/gigatl"
-tartemplate = "gigatl1_his_1h.2008-09-23.{subd:02}.tar"
+domain = os.popen("hostname -d").read()
+
+if "tgcc" in domain:
+    hostname = "irene"
+
+elif "univ-brest" in domain:
+    hostname = os.popen("hostname").read()[:-1]
+
+else:
+    raise ValueError("Could not find the Gigatl data")
+
+print(f"hostname: {hostname}")
+
+if hostname == "irene":
+    dirgridtar = "/ccc/store/cont003/gch0401/gch0401/GIGATL1_1h_tides/GRD/{subd:02}"
+
+    dirgiga = "/ccc/store/cont003/gch0401/gch0401/GIGATL1_1h_tides/HIS_1h/{subd:02}"
+
+    # fix_filename_on_store
+    # dirsurf = "/ccc/store/cont003/gch0401/gch0401/GIGATL1_1h_tides/SURF"
+
+    dirmounted = "/ccc/work/cont003/gen7638/groullet/gigatl"
+
+    dirgrid = dirmounted+"/GRD/{subd:02}"
+    dirhis = dirmounted+"/HIS/{subd:02}"
+    # or use directly
+    # dirgrid = "/ccc/scratch/cont003/ra4735/gulaj/GIGATL1/INIT_N100_100_100/GRD3"
+
+    hisindex = 36
+    targridtemplate = "gigatl1_grd_masked.{subd:02}.tar"
+    tarhistemplate = "gigatl1_his_1h.2008-09-26.{subd:02}.tar"
+
+
+else:
+    dirgrid = "/net/omega/local/tmp/1/gula/GIGATL1/GIGATL1_1h_tides/GRD"
+    dirsurf = "/net/omega/local/tmp/1/gula/GIGATL1/GIGATL1_1h_tides/SURF/gigatl1_surf.2008-05-23"
+    dirgiga = "/net/omega/local/tmp/1/gula/GIGATL1/GIGATL1_1h_tides/HIS_1h"
+    dirmounted = "/net/omega/local/tmp/1/roullet/gigatl"
+
+    dirhis = dirmounted+"/HIS/{subd:02}"
+    hisindex = 72
+    tarhistemplate = "gigatl1_his_1h.2008-09-23.{subd:02}.tar"
 
 
 def grdfiles(tile):
-    return f"{dirgrid}/gigatl1_grd_masked.{tile:04}.nc"
+    subd = subdmap[tile]
+    directory = dirgrid.format(subd=subd)
+    filename = f"{directory}/gigatl1_grd_masked.{tile:04}.nc"
+    return filename
 
 
 def surffiles(tile):
@@ -32,24 +78,23 @@ def surffiles(tile):
 
 
 def hisfiles(tile):
-    # add time in the arguments => 000072
-    if tile in subdmap.keys():
-        subd = subdmap[tile]
-        filename = f"{dirmounted}/{subd:02}/gigatl1_his.000072.{tile:04}.nc"
-    else:
-        filename = ""
+    subd = subdmap[tile]
+    directory = dirhis.format(subd=subd)
+    filename = f"{directory}/gigatl1_his.{hisindex:06}.{tile:04}.nc"
     return filename
 
 
-def get_subdmap():
-    """Reconstruct how netCDF files are stored in *.tar files"""
-    subdmap = {}
+def get_subdmap(directory):
+    """Reconstruct how netCDF files are stored in fused directory
+
+    directory == dirgrid | dirhis """
+    _subdmap = {}
     for subd in subdomains:
-        fs = glob.glob(f"{dirmounted}/{subd:02}/*nc")
+        fs = glob.glob(directory.format(subd=subd)+"/*.nc")
         tiles = [int(f.split(".")[2]) for f in fs]
         for t in tiles:
-            subdmap[t] = subd
-    return subdmap
+            _subdmap[t] = subd
+    return _subdmap
 
 
 def mount_tar(source, template, subd, dest, overwrite=False):
@@ -72,7 +117,7 @@ def mount_tar(source, template, subd, dest, overwrite=False):
     assert len(whichmount) > 0, msg
 
     tomount = True
-    destdir = f"{dest}/{subd:02}"
+    destdir = f"{dest}"
     if os.path.exists(destdir):
         print(f"{destdir} already exists")
         if len(os.listdir(f"{destdir}")) == 0:
@@ -95,25 +140,38 @@ def mount_tar(source, template, subd, dest, overwrite=False):
         os.system(command)
 
 
-def mount(subd):
+def mount(subd, grid=False):
     """Mount tar file `subd`"""
-    destdir = f"{dirmounted}"
-    mount_tar(dirgiga, tartemplate, subd, destdir)
+    if grid:
+        destdir = dirgrid.format(subd=subd)
+        srcdir = dirgridtar.format(subd=subd)
+        template = targridtemplate
+
+    else:
+        destdir = dirhis.format(subd=subd)
+        srcdir = dirgiga.format(subd=subd)
+        template = tarhistemplate
+
+    mount_tar(srcdir, template, subd, destdir)
 
 
-def mount_all():
+def mount_all(grid=False):
     for subd in subdomains:
-        mount(subd)
+        mount(subd, grid=grid)
 
 
-def mount_stats():
+def mount_stats(grid=False):
     """ Print statistics on mounted tar files"""
 
     print("-"*40)
     print(BB("statistics on mounted tar files"))
     print(f"mounting point: {dirmounted}")
+
     for subd in subdomains:
-        destdir = f"{dirmounted}/{subd:02}"
+        if grid:
+            destdir = dirgrid.format(subd=subd)
+        else:
+            destdir = dirhis.format(subd=subd)
         if os.path.exists(destdir):
             filelist = os.listdir(f"{destdir}")
             nbfiles = len(filelist)
@@ -122,27 +180,35 @@ def mount_stats():
                 nbtiles = len(tiles)
                 tile = list(tiles)[0]
                 fs = [f for f in filelist if f"{tile:04}.nc" in f]
-                dateindex = sorted([int(f.split(".")[-3]) for f in fs])
-                nbdates = len(dateindex)
-                print(
-                    f"  - {subd:02} : {nbtiles:03} tiles x {dateindex} dateindex")
+                if grid:
+                    msg = f"  - {subd:02} : {nbtiles:03} tiles"
+                else:
+                    dateindex = sorted([int(f.split(".")[-3]) for f in fs])
+                    nbdates = len(dateindex)
+                    msg = f"  - {subd:02} : {nbtiles:03} tiles x {dateindex} dateindex"
         else:
             warning = BB("destroyed")
-            print(f"  - {subd:02} : {warning}")
+            msg = f"  - {subd:02} : {warning}"
+        print(msg)
 
 
-def umount_all():
+def umount_all(grid=False):
     for subd in subdomains:
-        umount(subd)
+        umount(subd, grid=grid)
 
 
-def umount(subd):
+def umount(subd, grid=False):
     """ Unmount `subd` tar archive folder
 
     The command to unmount a fuse folder is fusermount -u"""
 
-    destdir = f"{dirmounted}/{subd:02}"
-    if len(os.listdir(f"{destdir}")) != 0:
+    if grid:
+        destdir = dirgrid.format(subd=subd)
+
+    else:
+        destdir = dirhis.format(subd=subd)
+
+    if os.path.isdir(destdir) and len(os.listdir(f"{destdir}")) != 0:
         command = f"fusermount -u {destdir}"
         os.system(command)
         command = f"rmdir {destdir}"
