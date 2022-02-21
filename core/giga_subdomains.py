3cbea5c38cd395892a93c272204b0cee9a700196
gula <Mesharou@gmail.com>
2022-02-21 19:00:16 +0100

    Add a function to get all tiles needed to fill a square domain including targeted region

--- core/giga_subdomains.py
+++ core/giga_subdomains.py
@@ -14,6 +14,25 @@ def find_tiles_inside(param, domain, oceanonly=True):
                 tileslist += [tile]
     return tileslist
 
+
+def square_domain(param,tileslist):
+    """
+       add tiles needed to get a squared domain
+
+    """
+    rows = [tile//100 for tile in tileslist]
+    cols = [tile%100 for tile in tileslist]
+    
+    square_tileslist = []
+    for col in range(min(cols),max(cols)+1):
+        for row in range(min(rows),max(rows)+1):
+            mytile = row*100 +col
+            if not param.missing[mytile]:
+                square_tileslist += [mytile]
+
+    return square_tileslist
+
+
 def LLTR2domain(lowerleft, topright):
     """Convert the two pairs of (lower, left), (top, right) in (lat, lon)
     into the four pairs of (lat, lon) of the corners """
